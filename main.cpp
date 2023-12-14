fe73dfcc9a9829069bc1c38a7b803a0c860c04da
ningshuncai <50795830@qq.com>
2023-12-14 22:18:23 +0800

    main

--- main.cpp
+++ main.cpp
@@ -7,23 +7,42 @@ void printValue(people pp){
     std::cout << pp.age<<"  "<<pp.ll << std::endl;
 }
 
+void printValueBB(people pp){
+    std::cout << pp.age<<"  "<<pp.ll << "BB"<< std::endl;
+}
+
+struct mat{
+    std::string type="mat";
+    int age=0;
+};
+void printValueAA(mat pp){
+    std::cout << pp.age<<"  "<<pp.type << std::endl;
+}
+
 
 int main() {
-    Person::getInstance().registerHandler<people>(std::bind(printValue, std::placeholders::_1));
+    EventManager::getInstance().registerHandler<people>(std::bind(printValue, std::placeholders::_1));
+    EventManager::getInstance().registerHandler<people>(std::bind(printValueBB, std::placeholders::_1));
+    EventManager::getInstance().registerHandler<mat>(std::bind(printValueAA, std::placeholders::_1));
     people ljj;
     ljj.age =18;
     ljj.ll =100;
-    Person::getInstance().post(ljj);
+    EventManager::getInstance().post(ljj);
+
+    mat m;
+    m.type = "Mat";
+    m.age = 1000;
+    EventManager::getInstance().post(m);
 
     ljj.age =28;
     ljj.ll =200;
-    Person::getInstance().post(ljj);
+    EventManager::getInstance().post(ljj);
 
-    Person::getInstance().unregisterHandler<people>();
+    EventManager::getInstance().unregisterHandler<people>();
 
     ljj.age =38;
     ljj.ll =300;
-    Person::getInstance().post(ljj);
+    EventManager::getInstance().post(ljj);
     return 0;
 }
 
